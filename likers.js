1845b729cd1fdec29f4329ecb6cf89592e16c5fc
Philippe Thronte <lipelip@gmail.com>
2014-02-19 11:14:00 +0100

    Fix
    span > button
    total value

--- likers.js
+++ likers.js
@@ -5,7 +5,7 @@ function addLikers(container){
 
     var value = Number(el.dataset.value) || 0;
 
-    var likeBtn = document.createElement('span');
+    var likeBtn = document.createElement('button');
     likeBtn.className = 'liker-like-btn';
     likeBtn.innerText = '✔';
     likeBtn.addEventListener('click', function(e){
@@ -17,9 +17,9 @@ function addLikers(container){
       updateElements();
     });
 
-    var dislikeBtn = document.createElement('span');
+    var dislikeBtn = document.createElement('button');
     dislikeBtn.innerHTML = "✘";
-    dislikeBtn.className = 'liker-like-btn';
+    dislikeBtn.className = 'liker-dislike-btn';
     //dislikeBtn.innerText = '';
     dislikeBtn.addEventListener('click', function(e){
       if ( value === -1 ) {
@@ -30,24 +30,37 @@ function addLikers(container){
       updateElements();
     });
 
-    function total(){
-	    
-	    //recuperer span.total
-    }
-
+    var totalLike = document.createElement('span'),
+     		totaldisLike = document.createElement('span');
+     		
+		totalLike.className = "total-like";
+		likeBtn.appendChild(totalLike);
+		
+		totaldisLike.className = "total-dislike";
+		dislikeBtn.appendChild(totaldisLike);
+		
+		//Un nombre pour l'exemple 
+		var totalValue = "5";
+ 
     function updateElements(){
       switch( value ) {
         case 0:
           likeBtn.classList.remove("selected");
           dislikeBtn.classList.remove("selected");
+          totalLike.innerHTML = totalValue;
+          totaldisLike.innerHTML = totalValue;
           break;
         case 1:
           likeBtn.classList.add("selected");
           dislikeBtn.classList.remove("selected");
+          totalLike.innerHTML = parseInt(totalValue) + 1;
+          totaldisLike.innerHTML = parseInt(totalValue) - 1;
           break;
         case -1:
           likeBtn.classList.remove("selected");
           dislikeBtn.classList.add("selected");
+          totalLike.innerHTML = parseInt(totalValue) - 1;
+          totaldisLike.innerHTML = parseInt(totalValue) + 1;
           break;
       }
 
